ae168cba230559dd71368cdd84217ec15194a786
AcademyAspNet <208851284+AcademyAspNet@users.noreply.github.com>
2026-03-02 21:35:13 +0300

    Added Person class

---
+++ src/Person.java
@@ -0,0 +1,66 @@
+public class Person {
+
+    private String fullName;
+    private String dateOfBirth;
+    private String phoneNumber;
+    private String city;
+    private String country;
+    private String address;
+
+    public Person(String fullName, String dateOfBirth, String phoneNumber, String city, String country, String address) {
+        this.fullName = fullName;
+        this.dateOfBirth = dateOfBirth;
+        this.phoneNumber = phoneNumber;
+        this.city = city;
+        this.country = country;
+        this.address = address;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
